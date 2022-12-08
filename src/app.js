19c8b5b5fa084b7f5624b84112d4fadd64546a75
Ivan Hryhoryeu <ivan.grigoryev502@gmail.com>
2022-12-08 22:08:50 +0200

    feat: exiting with message

--- src/app.js
+++ src/app.js
@@ -2,13 +2,17 @@ import { parseArgs } from "./cli.js";
 import {
     USERNAME_PARAM,
     DEFAULT_USERNAME,
-    WELCOME_MESSAGE_TEMPLATE
-} from "./constants.js"
+    WELCOME_MESSAGE_TEMPLATE,
+    EXIT_MESSAGE_TEMPLATE
+} from "./constants.js";
+import createInterface from 'readline';
+
 
 class App {
     constructor() {
         this.input = this.input.bind(this);
         this.run = this.run.bind(this);
+        this.teardown = this.teardown.bind(this);
     };
 
     run() {
@@ -24,14 +28,43 @@ class App {
         this.say(welcomeMsg);
 
         process.stdin.on('data', this.input)
+        
+        if (process.platform === "win32") {
+            const rl = require("readline").createInterface({
+                input: process.stdin,
+                output: process.stdout
+            });
+          
+            rl.on("SIGINT", function () {
+                process.emit("SIGINT");
+            });
+        }
+          
+        process.on("SIGINT", this.teardown);
     };
 
-    input (msg) {
-        this.say(msg);
+    input (buf) {
+        let msg = this._decodeBuffer(buf)
+        if (msg == '.exit')
+            this.teardown();
+        else
+            this.say(msg);
     };
 
     say (msg) {
         process.stdout.write(msg + '\n');
+    };
+
+    teardown (exitCode = 0) {
+        let exitMsg = EXIT_MESSAGE_TEMPLATE.replace(
+            '{username}', this.username
+        );
+        this.say('\n' + exitMsg);
+        process.exit(exitCode);
+    };
+
+    _decodeBuffer (str) {
+        return str.toString().replace(/(\r\n|\n|\r)/gm, "");
     };
 };
 
--- src/constants.js
+++ src/constants.js
@@ -2,3 +2,4 @@ export const USERNAME_PARAM = 'username';
 export const DEFAULT_USERNAME = 'amogus';
 
 export const WELCOME_MESSAGE_TEMPLATE = 'Welcome to the File Manager, {username}!';
+export const EXIT_MESSAGE_TEMPLATE = 'Thank you for using File Manager, {username}, goodbye!';
