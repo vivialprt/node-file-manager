d497431c2736e2c0b00715946bd8522c7a4ee7e7
Ivan Hryhoryeu <ivan.grigoryev502@gmail.com>
2022-12-16 19:16:01 +0200

    feat: os --cpus

--- src/os.js
+++ src/os.js
@@ -4,7 +4,14 @@ import { inspect } from 'util';
 export const getOsInfo = async (param) => {
     switch (param) {
         case '--EOL':
-            return inspect(os.EOL, { showHidden: true });
+            return inspect(os.EOL, { showHidden: true }) + '\n';
+
+        case '--cpus':
+            let info = os.cpus()
+            let total = `Total: ${info.length}`;
+            info = info.map(core => `Model: ${core.model} Clock Rate: ${Math.round((core.speed * 0.001 + Number.EPSILON) * 1000) / 1000} GHz`);
+            info = [total, ...info].join('\n');
+            return info + '\n';
 
         default:
             throw new Error();
