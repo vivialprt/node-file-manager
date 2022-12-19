a7117022c26a7e512fa5bdfe66ee53e9de81e0d2
Ivan Hryhoryeu <Ivan.Hryhoryeu@itechart-group.com>
2022-12-19 12:01:31 +0200

    ref: single function for compress and decompress

--- src/zip.js
+++ src/zip.js
@@ -5,32 +5,15 @@ import { promisify } from 'node:util';
 import path from 'path';
 
 
-export const compress = async (cwd, src, dst) => {
-    const fullSrc = path.isAbsolute(src) ? src : path.join(cwd, src);
-    const fullDst = path.isAbsolute(dst) ? dst : path.join(cwd, dst);
+export const createZipFunction = async (cwd, src, dst, mode = 'compress') => {
+    let compressStream;
+    if (mode === 'compress')
+        compressStream = createBrotliCompress();
+    else if (mode === 'decompress')
+        compressStream = createBrotliDecompress();
+    else
+        throw new Error();
 
-    const pipe = promisify(pipeline);
-
-    const readStream = createReadStream(fullSrc);
-    await new Promise((resolve, reject) => {
-        readStream.on('open', resolve);
-        readStream.on('error', reject);
-    });
-
-    const writeStream = createWriteStream(fullDst, { flags: 'wx' });
-    await new Promise((resolve, reject) => {
-        writeStream.on('open', resolve);
-        writeStream.on('error', reject);
-    });
-
-    const compressStream = createBrotliCompress();
-
-    await pipe(readStream, compressStream, writeStream);
-
-};
-
-
-export const decompress = async (cwd, src, dst) => {
     const fullSrc = path.isAbsolute(src) ? src : path.join(cwd, src);
     const fullDst = path.isAbsolute(dst) ? dst : path.join(cwd, dst);
 
@@ -48,8 +31,5 @@ export const decompress = async (cwd, src, dst) => {
         writeStream.on('error', reject);
     });
 
-    const compressStream = createBrotliDecompress();
-
     await pipe(readStream, compressStream, writeStream);
-
 };
